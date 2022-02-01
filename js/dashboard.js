4d5665f9876ea45db89ad2efd4a18efff7d6a28b
Dele O <dele@authoritywit.com>
2022-02-01 10:12:11 -0500

    adds the news delete feature to the project

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,59 +1,84 @@
-let allNews = [{
-    fullName: "John Doe",
-    date: "2021-12-01",
-    content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa delectus facilis numquam!"
-}, {
-    fullName: "Mary Doe",
-    date: "2021-12-02",
-    content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa delectus facilis numquam!"
-}, {
-    fullName: "Annete Doe",
-    date: "2021-12-22",
-    content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa delectus facilis numquam!"
-}
-];
+let newsFromBackendDataStore = [
+    {
+        id: 1,
+        fullName: "John Doe",
+        date: "2021-12-01",
+        content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa delectus facilis numquam!"
+    }, {
+        id: 2,
+        fullName: "Mary Doe",
+        date: "2021-12-02",
+        content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa delectus facilis numquam!"
+    }, {
+        id: 3,
+        fullName: "Annete Doe",
+        date: "2021-12-22",
+        content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa delectus facilis numquam!"
+    }];
+
 const newsShowEl = document.getElementById('news-show');
 const newsForm = document.getElementById('news-form');
 
-displayAllNews(allNews);
+displayAllNews(newsFromBackendDataStore);
 
-
-// event listener
+// Event Listener
 newsForm.addEventListener('submit', (event) => {
     event.preventDefault();
 
-    // news-submitted-by
-    console.log(document.getElementById('news-submitted-by').value);
-    console.log(document.getElementById('news-content').value);
-
     const newNews = {
+        id: Math.floor(Math.random() * 1000),
         fullName: document.getElementById('news-submitted-by').value,
         date: "2022-01-26",
         content: document.getElementById('news-content').value
     };
 
-    allNews = [...allNews, newNews]; //spread
+    newsFromBackendDataStore = [newNews, ...newsFromBackendDataStore];
 
-    displayAllNews(allNews);
+    displayAllNews(newsFromBackendDataStore);
 
 });
 
 // -- loop to show all news items
-
+/**
+ *
+ * @param {array} items
+ */
 function displayAllNews(items) {
     newsShowEl.innerHTML = "";
 
-    items.forEach(
-        item => addNewCard(item, newsShowEl) //anonymous functions
+    items.map(
+        item => addNewCard(item, newsShowEl)
     );
+}
+
+function deleteNews(newsId) {
+
+    // console.log(`you clicked news ID: ${newsId}`)
+
+    // console.log(newsFromBackendDataStore)
+
+    const filteredNews = newsFromBackendDataStore.filter(item => item.id !== newsId)
+
+    // store the new filter news.... storeNews(filterNews)
+    newsFromBackendDataStore = filteredNews
+
+    displayAllNews(newsFromBackendDataStore)
+
+    console.log(filteredNews)
+
 }
 
 function addNewCard(item, displayEl) {
     const newsCardDiv = document.createElement('div');
 
     newsCardDiv.innerHTML = `
-          <div class="pb-3 border-b-2 border-gray-500 ">
-           <h3> ${item.fullName} </h3>
+          <div class="p-3 m-2 bg-gray-200 shadow-sm">
+          <span class="mb-2 bg-black text-white w-2 p-2 rounded-full cursor-pointer"
+            onclick="deleteNews(${item.id})"
+          >
+          ⨯
+          </span>
+           <h3> ID: ${item.id} | ${item.fullName} </h3>
             <h5 class="text-xs text-gray-600"> ${item.date} </h5>
             <p class="text-xs"> ${item.content} </p>
         </div>
